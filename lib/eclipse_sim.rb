593149e559252574d73f0f76bc875acd8ceacc03
Evan Dorn <evan@lrdesign.com>
2013-06-11 23:52:12 -0700

    Refactor DieRoll to use dependency injection (with default) for die rolls

--- lib/eclipse_sim.rb
+++ lib/eclipse_sim.rb
@@ -2,5 +2,7 @@ file = File.expand_path("../**/*.rb", __FILE__)
 Dir[file].each {|f| require f}
 
 module EclipseSim
+
+
 end
 
--- lib/eclipse_sim/die_roll.rb
+++ lib/eclipse_sim/die_roll.rb
@@ -3,9 +3,10 @@ module EclipseSim
     attr_reader :damage, :computer_power, :value
 
     def initialize(weapon, ship)
+
       @damage = weapon.damage
       @computer_power = ship.computer_power
-      @value = Kernel.rand(1..6)
+      @value = @@roller.next  #Kernel.rand(1..6)
     end
 
     def can_hit?(target)
@@ -16,5 +17,30 @@ module EclipseSim
     def to_s
       "#{@value}(+#{@computer_power})" + '*' * damage
     end
+
+    def self.set_roller(roller)
+      @roller = roller
+    end
+
+    class RandRoller
+      def next
+        Kernel.rand(1..6)
+      end
+    end
+
+    class FixedRoller
+      def initialize(sequence = nil)
+        @sequence = sequence or [1,2,3,4,5,6]
+        @position = 0
+      end
+      def next
+        val = @sequence[@position]
+        @position += 1
+        @position = @position % @sequence.length
+        val
+      end
+    end
+
+    @@roller = RandRoller.new
   end
 end
